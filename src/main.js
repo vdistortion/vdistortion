a9c3a97fb9b37c651cd61882271397978de86192
Валентин <astrotrain55@gmail.com>
2022-12-26 15:01:58 +0500

    added masonry

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import VueParticles from 'vue-particles';
+import MasonryWall from '@yeger/vue-masonry-wall';
 import App from './App.vue';
 import router from './router';
 import { YoutubeAPI } from './plugins/youtube';
@@ -10,5 +11,6 @@ createApp(App)
   .use(createPinia())
   .use(router)
   .use(VueParticles)
+  .use(MasonryWall)
   .provide(YoutubeAPI.name, YoutubeAPI)
   .mount('#app')
