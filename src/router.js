735e6aa499a1a4b46d32365da3451036965919ab
Валентин <astrotrain55@gmail.com>
2023-04-05 19:25:09 +0500

    moving kira-sekira.ru

--- src/router.js
+++ src/router.js
@@ -14,15 +14,5 @@ export default createRouter({
       name: 'project-view',
       component: () => import('./views/ProjectView.vue'),
     },
-    {
-      path: '/Yulianna',
-      name: 'yulianna-home',
-      component: () => import('./Yulianna/views/HomeView.vue'),
-    },
-    {
-      path: '/Yulianna/:id',
-      name: 'yulianna-view',
-      component: () => import('./Yulianna/views/ProjectView.vue'),
-    },
   ],
 });
