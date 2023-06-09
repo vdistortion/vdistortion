8e71f2e130762473a97f62ef1d57fb97450d62de
Валентин Золотов <astrotrain55@gmail.com>
2023-06-10 01:21:52 +0500

    rm directory-structure-json

--- dir-to-json.js
+++
@@ -1,7 +0,0 @@
-import DirectoryStructureJSON from 'directory-structure-json';
-import fs from 'fs';
-
-DirectoryStructureJSON.getStructure(fs, 'public/images/iguana/', (err, structure) => {
-  if (err) console.log(err);
-  console.log(JSON.stringify(structure, null, 2));
-});
