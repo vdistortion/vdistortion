2192cfa1eed28f9f41fe5ba57b558018f4169ae5
Валентин <astrotrain55@gmail.com>
2024-02-04 09:25:03 +0500

    update projects

---
+++ src/projects/modules/creablast.ts
@@ -0,0 +1,25 @@
+export default {
+  code: 'creablast',
+  name: 'Creablast',
+  description: 'Портфолио в розовых тонах',
+  links: [
+    {
+      name: 'Сайт',
+      url: '/creablast/',
+    },
+  ],
+  tags: [
+    'pet-project',
+    'angular',
+    'scss',
+    'smartgrid',
+    'markup',
+    'typescript',
+    'vite',
+  ],
+  imageNames: {
+    '1.png': 'Main Page',
+    '2.png': 'Work Page',
+    '3.png': 'Project Page',
+  },
+};
