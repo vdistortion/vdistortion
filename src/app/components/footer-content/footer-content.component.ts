2942d72d54dc77e1c99f02b395b7457b5cd987b9
Валентин <astrotrain55@gmail.com>
2024-06-07 11:53:41 +0500

    refactor footer

--- src/app/components/footer-content/footer-content.component.ts
+++ src/app/components/footer-content/footer-content.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { UiCardComponent } from '../ui/ui-card/ui-card.component';
 import { NgClass } from '@angular/common';
 
+type Item = {
+  name: string;
+  link: string;
+};
+
 @Component({
   selector: 'app-footer-content',
   standalone: true,
@@ -9,4 +14,23 @@ import { NgClass } from '@angular/common';
   templateUrl: './footer-content.component.html',
   styleUrl: './footer-content.component.scss',
 })
-export class FooterContentComponent {}
+export class FooterContentComponent {
+  public list: Item[] = [
+    {
+      name: 'GitHub',
+      link: 'https://github.com/astrotrain55',
+    },
+    {
+      name: 'npmjs',
+      link: 'https://www.npmjs.com/~astrotrain',
+    },
+    {
+      name: 'Telegram',
+      link: 'https://t.me/valentin_fatherland',
+    },
+    {
+      name: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/valentin-zolotov/',
+    },
+  ];
+}
